b2ae5dcd4cf23261d3265ab41f391d9628669479
Otutsukihyuuga <111prathamshah@gmail.com>
2021-10-14 11:22:09 +0530

    added buggy code

--- App.js
+++ App.js
@@ -5,8 +5,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.expected}>proper dashed</Text>
+      <Text style={styles.bug}>solid even though added dashed but no borderRadius</Text>
+      <Text style={styles.bugTwo}>solid even though added dashed added borderRadius as 0</Text>
     </View>
   );
 }
@@ -14,8 +15,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 10,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  expected: {
+    padding: 20,
+    marginBottom: 20,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderRadius: 1,
+  },
+  bug: {
+    padding: 20,
+    marginBottom: 20,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderStyle: 'dashed',
+  },
+  bugTwo: {
+    padding: 20,
+    marginBottom: 20,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderRadius: 0,
   },
 });
